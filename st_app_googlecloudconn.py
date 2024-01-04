69d60eace99a16916cf754dbdda9a9aca57cd382
aembaye <abel.embaye@gmail.com>
2024-01-04 14:01:48 -0600

    Update st_app_googlecloudconn.py

--- st_app_googlecloudconn.py
+++ st_app_googlecloudconn.py
@@ -1,9 +1,7 @@
-import json
 import streamlit as st
 from google.cloud import storage
 import pandas as pd
 import gcsfs
-import os
 
 # Fetch the secret
 GCS_SERVICE_ACCOUNT_JSON = st.secrets["GCS_SERVICE_ACCOUNT"]
