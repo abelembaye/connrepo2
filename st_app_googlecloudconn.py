549acf3dd95f58b7331c5ac74e34ae9576d6966d
aembaye <abel.embaye@gmail.com>
2024-01-04 19:17:58 -0600

    Update st_app_googlecloudconn.py

--- st_app_googlecloudconn.py
+++ st_app_googlecloudconn.py
@@ -14,7 +14,8 @@ from st_files_connection import FilesConnection
 # Create connection object and retrieve file contents.
 # Specify input format is a csv and to cache the result for 600 seconds.
 conn = st.connection('gcs', type=FilesConnection)
-df = conn.read("streamlit-bucket/myfile.csv", input_format="csv", ttl=600)
+#df = conn.read("streamlit-bucket/myfile.csv", input_format="csv", ttl=600)
+df = conn.read("streamlit-bucket/myfile.csv", input_format="csv", ttl=0)
 
 # Print results.
 for row in df.itertuples():
