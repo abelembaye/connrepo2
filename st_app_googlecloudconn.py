ccbc03d1654cac9de27f90b6f20381691a071a88
aembaye <abel.embaye@gmail.com>
2024-01-04 19:49:16 -0600

    Update st_app_googlecloudconn.py

--- st_app_googlecloudconn.py
+++ st_app_googlecloudconn.py
@@ -5,14 +5,24 @@ import gcsfs
 import json
 from st_files_connection import FilesConnection
 
-# Create connection object and retrieve file contents.
-# Specify input format is a csv and to cache the result for 600 seconds.
-conn = st.connection('gcs', type=FilesConnection)
-#df = conn.read("hw3_bucket2/myfile.csv", input_format="csv", ttl=600)
-df = conn.read("hw3_bucket2/myfile.csv", input_format="csv", ttl=0)
+import streamlit as st
+import pandas as pd
+import gcsfs
+
+# Create a connection to GCS
+# fs = gcsfs.GCSFileSystem(project='black-heading-405002',
+#                          token='black-heading-405002-e11b4e15b0e5.json')
+
+#st.cache.clear()
+fs = gcsfs.GCSFileSystem(token='black-heading-405002-e11b4e15b0e5.json')
+
+# Read the CSV file from GCS into a pandas DataFrame
+with fs.open('hw3_bucket2/myfile2.csv') as f:
+    df = pd.read_csv(f)
 
 # Print results.
 for row in df.itertuples():
-    st.write(f"{row.Owner} has a :{row.Pet}:")
+    st.write(f"{row.username} has a :{row.password}:")
+
 
 
