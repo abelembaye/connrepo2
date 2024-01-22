0cc389bd7fa241b605986d99bbc2829bd98612e4
aembaye <abel.embaye@gmail.com>
2024-01-21 21:41:33 -0600

    Update st_app_googlecloudconn.py

--- st_app_googlecloudconn.py
+++ st_app_googlecloudconn.py
@@ -8,6 +8,8 @@ from st_files_connection import FilesConnection
 conn = st.connection('gcs', type=FilesConnection)
 df = conn.read("hw3_bucket2/myfile.csv", input_format="csv", ttl=5)
 
+st.write(df)
+
 # Print results.
 for row in df.itertuples():
     st.write(f"{row.Owner} has a :{row.Pet}:")
